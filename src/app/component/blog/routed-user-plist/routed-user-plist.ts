5ef3ef724ed0f840bd67fa5a098c5e118b497181
Rafael Aznar <rafaaznar@gmail.com>
2025-12-17 10:51:59 +0100

    Add conditional rendering for blog posts and alert for no posts available

--- src/app/component/blog/routed-user-plist/routed-user-plist.ts
+++ src/app/component/blog/routed-user-plist/routed-user-plist.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IPage } from '../../../model/plist';
 import { IBlog } from '../../../model/blog';
 import { BlogService } from '../../../service/blog';
@@ -9,7 +10,7 @@ import { UnroutedUserView2 } from "../unrouted-user-view2/unrouted-user-view2";
 
 @Component({
   selector: 'app-routed-user-plist',
-  imports: [Paginacion, UnroutedUserView2],
+  imports: [CommonModule, Paginacion, UnroutedUserView2],
   templateUrl: './routed-user-plist.html',
   styleUrl: './routed-user-plist.css',
 })
@@ -17,6 +18,8 @@ export class RoutedUserPlist {
   oPage: IPage<IBlog> | null = null;
   numPage: number = 0;
   numRpp: number = 2;
+  // contador actual de elementos en la tabla pública
+  totalElementsCount: number = 0;
 
   constructor(private oBlogService: BlogService) { }
 
@@ -30,6 +33,7 @@ export class RoutedUserPlist {
     this.oBlogService.getPage(this.numPage, this.numRpp, 'fechaCreacion', 'desc').subscribe({
       next: (data: IPage<IBlog>) => {
         this.oPage = data;
+        this.totalElementsCount = data.totalElements ?? 0;
         // OJO! si estamos en una página que supera el límite entonces nos situamos en la ultima disponible
         if (this.numPage > 0 && this.numPage >= data.totalPages) {
           this.numPage = data.totalPages - 1;
